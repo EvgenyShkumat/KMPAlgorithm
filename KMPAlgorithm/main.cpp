2e01b01de72b2c6254a57d89d95bec39a16f3c86
Евгений Шкумат <kotbmasle@gmail.com>
2024-09-12 23:32:51 +0300

    Add project files.

---
+++ KMPAlgorithm/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int KMP(string base, string s);
+
+int main() {
+
+	cout << KMP("aaapple", "apple");
+
+	return 0;
+}
+
+int KMP(string base, string s) {
+	int* p = new int[s.length()];
+	*p = 0;
+	int i = 1, j = 0;
+
+
+	for (; i < s.length();)
+	{
+		if (s[i] == s[j]) {
+			*(p + i) = j + 1;
+			j++;
+			i++;
+		} 
+		else if (j == 0) {
+			*(p + i) = 0;
+			i++;
+		}
+		else {
+			j = p[j - 1];	
+		}
+	}
+
+	i = 0, j = 0;
+
+	while(i < base.length()) {
+		if (base[i] == s[j]) {
+			i++;
+			j++;	
+		}
+		if (j == s.length() - 1) {
+			return i - j;
+		}
+		else if (i < base.length() && s[j] != base[i]) {
+			if (j != 0) {
+				j = p[j - 1];
+			}
+			else {
+				i++;
+			}
+		}
+		
+	}
+
+	return -1;	
+}
